cf9f3dfe6248ffe11505ea463854dfe90864df1f
pemauric <pedro.a.mauricio1@gmail.com>
2023-08-28 11:28:45 -0300

    added middleware cors in app.js

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
 const port = 3000
 
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors({
+    credentials: 'true',
+    origin: 'http://localhost:3000'
+}))
 
+app.use(express.static('public'))
 
-const PetRoutes = require('./routes/pet.routes')
-const UserRoutes = require('./routes/user.routes')
+//const PetRoutes = require('./routes/pet.routes')
+//const UserRoutes = require('./routes/user.routes')
 
-app.use('/pets', PetsRoutes)
-app.use('/users', UserRoutes)
+//app.use('/pets', PetRoutes)
+//app.use('/users', UserRoutes)
 
 
 app.listen(port, () => {
